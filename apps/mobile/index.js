c8ef7b9abbf0dd56a8851e8177f8de4cd318e09b
Elvis Nunez <elvisnunez@me.com>
2025-11-07 20:12:04 +0100

    Mobile: wrap App with SafeAreaProvider to support react-native-safe-area-context

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,8 +1,16 @@
 /**
  * @format
  */
+const React = require('react')
 const {AppRegistry} = require('react-native')
+const { SafeAreaProvider } = require('react-native-safe-area-context')
 const App = require('./App').default
 const {name: appName} = require('./app.json')
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => function Root() {
+  return React.createElement(
+    SafeAreaProvider,
+    null,
+    React.createElement(App, null)
+  )
+})
